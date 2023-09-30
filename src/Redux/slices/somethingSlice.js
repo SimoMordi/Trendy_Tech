7dcc35e0f4275cc2606b47057dac15d0b2865991
Learner_SZH3Z110 <Learner_SZH3Z110@PSWASZH3Z110>
2023-09-30 10:25:21 -0700

    n

--- src/Redux/slices/somethingSlice.js
+++ src/Redux/slices/somethingSlice.js
@@ -1,14 +1,14 @@
-import { createSlice } from "@reduxjs/toolkit";
+// import { createSlice } from "@reduxjs/toolkit";
 
 
 
-const somethingSlice = createSlice({
-    name: "something",
-    initialState:"" ,
-    reducers: {
-        showSomething: (currentState) => currentState = "Hello"
-    }
-})
+// const somethingSlice = createSlice({
+//     name: "something",
+//     initialState:"" ,
+//     reducers: {
+//         showSomething: (currentState) => currentState = "Hello"
+//     }
+// })
 
-export const {showSomething } = somethingSlice.actions
-export default somethingSlice.reducer
+// export const {showSomething } = somethingSlice.actions
+// export default somethingSlice.reducer
