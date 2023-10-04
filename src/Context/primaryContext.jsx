20e60cd16e37479a3576231afd4582193133aa8c
simo <simo.mordi@gmail.com>
2023-10-04 16:48:42 -0700

    day3

--- src/Context/primaryContext.jsx
+++ src/Context/primaryContext.jsx
@@ -1,18 +1,32 @@
-import { createContext } from "react";
-import fetchProducts from "../components/FetchProducts";
+import { createContext, useEffect, useState } from "react";
+import axios from "axios";
 
 // create context
-const ProductContext = createContext();
+export const productContext = createContext();
 // craete provider component
 const PrimaryProvider = ({ children }) => {
-    const { products } = fetchProducts();
+  const [products, setProducts] = useState([])
 
+  useEffect(() => {
+    // const apiKey = "26AFF168CBD24F7B944275C00CBFEBB8"
+    // const apiURL = `https://api.redcircleapi.com/request?api_key=${apiKey}&type=category&category_id=5xtg8`;
 
+    axios.get("https://fakestoreapi.com/products")
+      .then((response) => {
+       
+        console.log( response.data); 
+
+        const data = response.data;
+        setProducts(data);
+      })
+     
+  }, []);
+        
     return (
 
-        <ProductContext.Provider value={{ products }}>
+        <productContext.Provider value={{ products }}>
             {children}
-        </ProductContext.Provider>
+        </productContext.Provider>
     )
 }
-export { PrimaryProvider, ProductContext }
+export { PrimaryProvider }
--- src/pages/ProductDisplay/index.jsx
+++ src/pages/ProductDisplay/index.jsx
@@ -1,25 +1,25 @@
-
-
-import getProducts from '../../components/FetchProducts';
+import React, { useContext } from "react";
 import './index.css';
+import { productContext } from "../../Context/primaryContext";
 
 const ProductPage = () => {
-  const { products } = getProducts();
-
-  
+  const { products } = useContext(productContext);
 
   return (
     <div id="productDisplay">
       {products.map((product) => (
         <div key={product.id}>
-          <h3>Name: {product.name}</h3>
-          <h3>Maker: {product.maker}</h3>
+          <h3>{product.title}</h3>
+          <h3>Category: {product.category}</h3>
           <h3>Price: $ {product.price}</h3>
+          <img id="productImg" src={product.image} />
         </div>
       ))}
     </div>
   );
+  
+   
+  
 };
 
 export default ProductPage;
-
